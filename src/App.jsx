2b921c9c552e5ae3ee2ce52b6751a7d21e1ff379
moneebshammout <moneeb41@gmail.com>
2022-07-14 15:51:04 +0300

    test push

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import * as constants from './constants';
+
+function App() {
+  return <div style={{ color: constants.Dark_Blue }}>moneeb</div>;
+}
+//  nav color
+// "moneeb"
+export default App;
---
+++ src/constants.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable camelcase */
+/* eslint-disable no-unused-vars */
+const Dark_Blue = '#032541';
+
+export { Dark_Blue };
